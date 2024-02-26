260debf63292396d91eac47c34bdb77d9286c99f
Oluwatobi Adebola <tobi_newsystem@yahoo.com>
2024-02-26 15:50:20 +0100

    CTA responsiveness

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const CTA = () => {
   return (
     <div style={{ marginBottom: '240px' }}>
-    <div className="flex items-center justify-center rounded-full" style={{ width: '1200px', padding: '64px 166.6px', margin: '0 auto 48px', border: '1px solid #000000' }}>
+    <div className="flex items-center justify-center rounded-full cta">
       <div className="flex items-center justify-between w-full">
-        <p className="text-left" style={{ fontFamily: 'Montserrat', fontSize: '36px', fontWeight: '400', lineHeight: '44px' }}>Get your name change <br /> starting at $75*</p>
+        <p className="text-left cta-text">Get your name change <br /> starting at $75*</p>
         <div className="flex items-center justify-center mt-4 pb-8">
           <a href="#" className="px-8 py-4 rounded-full bg-gradient-to-r from-blue-900 to-blue-400 text-white uppercase font-inter text-base font-semibold leading-5">
             Start your name change!
@@ -14,7 +14,7 @@ const CTA = () => {
       </div>
     </div>
     <div className='flex items-center justify-center'>
-        <p style={{ fontFamily: 'Montserrat', fontSize: '18px', fontWeight: '400', lineHeight: '22px', color: '#101727' }}>*Price excludes court fees to be paid when you file your name change documents</p>
+        <p className='cta-note'>*Price excludes court fees to be paid when you file your name change documents</p>
     </div>
     </div>
   );
