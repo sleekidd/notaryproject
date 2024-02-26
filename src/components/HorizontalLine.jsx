a671edd267e9eca75d70b141d8fd54a40618c355
Oluwatobi Adebola <tobi_newsystem@yahoo.com>
2024-02-26 15:55:42 +0100

    CTA responsiveness

--- src/components/HorizontalLine.jsx
+++ src/components/HorizontalLine.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HorizontalLine = () => {
-  return <hr className="my-8 items-center justify-center" style={{ width: '100%', color: '#000000', border: '1px solid #000000' }}/>;
+  return <hr className="my-8 items-center justify-center horizontal-line" />;
 };
 
 export default HorizontalLine;
